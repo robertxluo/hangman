37e4305a1db51494ec1aa07e665b482919acec36
Robert <robertxluo@gmail.com>
2018-09-04 13:53:24 -0700

    Implement status of game

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const puzzle_element = document.querySelector("#puzzle");
 const guesses_element = document.querySelector("#guesses");
 const game1 = new Hangman('Cat', 2);
+
 puzzle_element.textContent = game1.getPuzzle();
 guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
+game1.calculateStatus();
+console.log(game1.status);
 
 console.log();
 
@@ -11,4 +14,6 @@ window.addEventListener("keypress", function (e) {
     game1.makeGuess(guess);
     puzzle_element.textContent = game1.getPuzzle();
     guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
+    game1.calculateStatus();
+    console.log(game1.status);
 })
--- hangman.js
+++ hangman.js
@@ -2,6 +2,24 @@ const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
     this.guessedLetters = [];
+    this.status = "playing";
+};
+
+Hangman.prototype.calculateStatus = function () {
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter));
+
+    if (this.remainingGuesses === 0)
+    {
+        this.status = "failed";
+    }
+    else if (finished)
+    {
+        this.status = "finished";
+    }
+    else
+    {
+        this.status = "playing";
+    }
 };
 
 Hangman.prototype.getPuzzle = function () {
