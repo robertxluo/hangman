13b9b65c74c753c58a864652600df360d659280c
Robert <robertxluo@gmail.com>
2018-09-04 13:28:10 -0700

    Refactor for readability

--- hangman.js
+++ hangman.js
@@ -27,18 +27,3 @@ Hangman.prototype.makeGuess = function(letter) {
         this.remainingGuesses--;
     }
 };
-
-const puzzle_element = document.querySelector("#puzzle");
-const guesses_element = document.querySelector("#guesses");
-const game1 = new Hangman('Cat', 2);
-puzzle_element.textContent = game1.getPuzzle();
-guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
-
-console.log();
-
-window.addEventListener("keypress", function (e) {
-    const guess = e.key;
-    game1.makeGuess(guess);
-    puzzle_element.textContent = game1.getPuzzle();
-    guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
-})
