a44c6bfff718cc8a18430739294cca38e2ca30bd
Robert <robertxluo@gmail.com>
2018-09-04 13:23:48 -0700

    Display game to browser instead of console

--- hangman.js
+++ hangman.js
@@ -28,12 +28,17 @@ Hangman.prototype.makeGuess = function(letter) {
     }
 };
 
+const puzzle_element = document.querySelector("#puzzle");
+const guesses_element = document.querySelector("#guesses");
 const game1 = new Hangman('Cat', 2);
-console.log(game1.getPuzzle());
-console.log(`You have: ${game1.remainingGuesses} guess(es) remaining`);
+puzzle_element.textContent = game1.getPuzzle();
+guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
+
+console.log();
 
 window.addEventListener("keypress", function (e) {
     const guess = e.key;
     game1.makeGuess(guess);
-    console.log(game1.getPuzzle());
+    puzzle_element.textContent = game1.getPuzzle();
+    guesses_element.textContent = `You have ${game1.remainingGuesses} guess(es) remaining.`;
 })
