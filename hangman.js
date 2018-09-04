8129fc7ce629ce6e8a37d0b022613cba86c46765
Robert <robertxluo@gmail.com>
2018-09-04 13:14:41 -0700

    Implement makeGuess method

--- hangman.js
+++ hangman.js
@@ -12,7 +12,28 @@ Hangman.prototype.getPuzzle = function () {
     return asterisks;
 };
 
-const guess1 = new Hangman('Jesus Christ', 5);
-const guess2 = new Hangman('Witch', 10);
-console.log(guess1.getPuzzle());
-console.log(guess2.getPuzzle());
+Hangman.prototype.makeGuess = function(letter) {
+    letter = letter.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(letter);
+    const isBadGuess = !this.word.includes(letter);
+    
+    if (isUnique)
+    {
+        this.guessedLetters.push(letter);
+    }
+
+    if (isUnique && isBadGuess)
+    {
+        this.remainingGuesses--;
+    }
+};
+
+const game1 = new Hangman('Cat', 2);
+console.log(game1.getPuzzle());
+console.log(`You have: ${game1.remainingGuesses} guess(es) remaining`);
+
+window.addEventListener("keypress", function (e) {
+    const guess = e.key;
+    game1.makeGuess(guess);
+    console.log(game1.getPuzzle());
+})
