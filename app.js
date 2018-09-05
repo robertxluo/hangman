40994593a88a21edc8980c6057b50bca6e9b88c3
Robert <robertxluo@gmail.com>
2018-09-05 11:42:44 -0700

    Complete integrating data into the game

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const puzzle_element = document.querySelector("#puzzle");
 const guesses_element = document.querySelector("#guesses");
 let game1;
 
-// puzzle_element.textContent = game1.puzzle;
-// guesses_element.textContent = game1.statusMessage;
-
-// window.addEventListener("keypress", (e) => {
-//     const guess = e.key;
-//     game1.makeGuess(guess);
-//     puzzle_element.textContent = game1.puzzle;
-//     guesses_element.textContent = game1.statusMessage;
-// });
+window.addEventListener("keypress", (e) => {
+    const guess = e.key;
+    game1.makeGuess(guess);
+    render();
+});
 
 const render = () => {
     puzzle_element.textContent = game1.puzzle;
