940a9138e317f57286794dac142b81c205b0f91a
Robert <robertxluo@gmail.com>
2018-09-05 11:40:32 -0700

    Add reset button to start new game

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 const puzzle_element = document.querySelector("#puzzle");
 const guesses_element = document.querySelector("#guesses");
-const game1 = new Hangman('Cat', 2);
+let game1;
 
-puzzle_element.textContent = game1.puzzle;
-guesses_element.textContent = game1.statusMessage;
+// puzzle_element.textContent = game1.puzzle;
+// guesses_element.textContent = game1.statusMessage;
 
-window.addEventListener("keypress", (e) => {
-    const guess = e.key;
-    game1.makeGuess(guess);
+// window.addEventListener("keypress", (e) => {
+//     const guess = e.key;
+//     game1.makeGuess(guess);
+//     puzzle_element.textContent = game1.puzzle;
+//     guesses_element.textContent = game1.statusMessage;
+// });
+
+const render = () => {
     puzzle_element.textContent = game1.puzzle;
     guesses_element.textContent = game1.statusMessage;
-});
+}
 
-getPuzzle("1").then((puzzle) => {
-    console.log(puzzle);
-}).catch((error) => {
-    console.log(`Error: ${error}`);
-});
+const startGame = async () => {
+    const puzzle = await getPuzzle("1");
+    game1 = new Hangman(puzzle, 5);
+    render();
+}
 
-getCurrentCountry().then((country) => {
-    console.log(country);
-}).catch((error) => {
-    console.log(error);
-});
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
+
+// getPuzzle("1").then((puzzle) => {
+//     console.log(puzzle);
+// }).catch((error) => {
+//     console.log(`Error: ${error}`);
+// });
+
+// getCurrentCountry().then((country) => {
+//     console.log(country);
+// }).catch((error) => {
+//     console.log(error);
+// });
